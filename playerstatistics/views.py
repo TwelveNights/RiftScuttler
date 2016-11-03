b1f6c932be111288c7a27b0afc3644f600a6d6b7
runqingz <Run@dhcp-206-87-212-81.ubcsecure.wireless.ubc.ca>
2016-11-03 12:54:44 -0700

    remove useless files

--- playerstatistics/views.py
+++ playerstatistics/views.py
@@ -14,18 +14,36 @@ def playdetail(request, pid):
         cursor.execute("SELECT name "
                        "FROM players "
                        "WHERE id = %s", [pid])
-        player = cursor.fetchone()
+        player = cursor.fetchall()
         cursor.execute("SELECT AVG(ps.kills) AS avgk, AVG(ps.deaths) AS avgd, AVG(ps.assists) AS avga "
                        "FROM players p, plays ps "
                        "WHERE p.id = %s AND p.id = ps.playerID ", [pid])
         avgkda = cursor.fetchall()
 
-        cursor.execute("SELECT MAX(ps.kills) AS avgk, MAX(ps.deaths) AS avgd, MAX(ps.assists) AS avga "
+        cursor.execute("SELECT MAX(ps.kills) AS maxk, MAX(ps.deaths) AS maxd, MAX(ps.assists) AS maxa "
                        "FROM players p, plays ps "
                        "WHERE p.id = %s AND p.id = ps.playerID ", [pid])
         maxkda = cursor.fetchall()
+        cursor.execute("DROP VIEW roleplaycount")
+        cursor.execute("CREATE VIEW roleplaycount AS "
+                       "SELECT ps.role,p.id ,COUNT(ps.role) AS rolecount "
+                       "FROM players p, plays ps "
+                       "WHERE p.id = ps.playerID "
+                       "GROUP BY ps.role ")
+        cursor.execute("SELECT r1.role "
+                       "FROM roleplaycount r1 "
+                       "WHERE r1.id = %s AND r1.rolecount >= "
+                       " (SELECT MAX(rolecount)"
+                       "  FROM roleplaycount r2"
+                       "  WHERE r2.id = %s)", [pid, pid])
+        role = cursor.fetchall()
         rank = calcranking(0,0,0,0,0,0)
-        html = "<html><body><p>the player showing now is <b>%s</b>.</p> <p>averageKDA:  </p></body></html>" % player
+        html = "<html><body><p>the player showing now is <b>%s</b>." \
+               "</p> <p>averageK/D/A:  %s/%s/%s </p>" \
+               "<p>maxK/D/A: %s/%s/%s </p>" \
+               "<p>most role played: %s </p>" \
+               "<p>rankPoint with the most played role: %s</body></html>"\
+               % (player[0][0], avgkda[0][0], avgkda[0][1], avgkda[0][2], maxkda[0][0], maxkda[0][1], maxkda[0][2], role, rank)
         return HttpResponse(html)
 
 
