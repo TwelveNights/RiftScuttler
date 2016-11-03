d9e4617093bd5539dfaba6f018a1ddef697b1b80
TwelveNights <eddiezhou1996@gmail.com>
2016-11-03 10:19:54 -0700

    Use dictionary fetcher

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.db import connection
 from django.http import HttpResponse
+from common import utils
+ 
 # Create your views here.
-
+ 
 def index(request):
     tournament = None
-    with connection.cursor() as c:
-        c.execute("SELECT * from tournaments")
-        tournament = c.fetchone()
-
-    print(tournament)
-
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * from tournaments")
+        tournament = utils.dictfetchall(cursor)
+ 
     return HttpResponse(tournament)
 
 def details(request, tournament_name, year):
@@ -21,3 +21,4 @@ def series(request, tournament_name, year, id):
 
 def match(request, tournament_name, year, id, number):
     return HttpResponse("You made it to match!")
+
