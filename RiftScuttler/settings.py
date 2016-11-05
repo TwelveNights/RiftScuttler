95f4bb99d3a9be267b60bc76be44e963f117ea36
Stephanie Chan <stephaniechan219@gmail.com>
2016-11-05 02:04:12 -0700

    load static files dynamically

--- RiftScuttler/settings.py
+++ RiftScuttler/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
 MEDIA_URL = "/media/"
