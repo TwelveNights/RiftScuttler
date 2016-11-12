32688b92d999d0921e316d00aeb2c6fa09dae1d3
Stephanie Chan <stephaniechan219@gmail.com>
2016-11-12 10:07:59 -0800

    redirect bug fix

--- curator/views.py
+++ curator/views.py
@@ -1,5 +1,6 @@
 from django.db import connection
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .forms import *
 from .helpers import *
@@ -14,66 +15,81 @@ from .helpers import *
 
 
 def add_data_page(request):
-    cursor = connection.cursor()
-    table = check_page_and_return_table(request)
-    if request.method == 'POST':
-        form = AccessFormInput(request.POST, extra=table.cols)
-        if form.is_valid():
-            for (attribute, value) in form.extra_attributes():
-                table.args.append([attribute, value])
-            table.args = reorder_dictionary(table)
-            insert_data(cursor, table)
-            return redirect(add_data_page, permanent=True)
-    form = AccessFormInput(extra=table.cols)
-    list_of_data = select_data(cursor, table.tname)
-    args = get_args(table.cols)
-    context = create_context(table.tname, form, list_of_data, args)
-    return render(request, 'curator/add.html', context)
+    if request.user.is_staff or request.user.is_superuser:
+        cursor = connection.cursor()
+        table = check_page_and_return_table(request)
+        if request.method == 'POST':
+            form = AccessFormInput(request.POST, extra=table.cols)
+            if form.is_valid():
+                for (attribute, value) in form.extra_attributes():
+                    table.args.append([attribute, value])
+                table.args = reorder_dictionary(table)
+                insert_data(cursor, table)
+                name = create_reverse_name_add(table.tname)
+                return redirect(reverse(name), permanent=True)
+        form = AccessFormInput(extra=table.cols)
+        list_of_data = select_data(cursor, table.tname)
+        args = get_args(table.cols)
+        context = create_context(table.tname, form, list_of_data, args)
+        return render(request, 'curator/add.html', context)
+    else:
+        return redirect(login_page, permanent=True)
 
 
 def remove_data_page(request):
-    cursor = connection.cursor()
-    table = check_page_and_return_table(request)
-    if request.method == 'POST':
-        form = AccessFormInput(request.POST, extra=table.pk)
-        if form.is_valid():
-            for (attribute, value) in form.extra_attributes():
-                table.args.append([attribute, value])
-            table.args = reorder_dictionary(table)
-            delete_data(cursor, table)
-            return redirect(remove_data_page, permanent=True)
-    form = AccessFormInput(extra=table.pk)
-    list_of_data = select_data(cursor, table.tname)
-    args = get_args(table.cols)
-    context = create_context(table.tname, form, list_of_data, args)
-    return render(request, 'curator/remove.html', context)
+    if request.user.is_staff or request.user.is_superuser:
+        cursor = connection.cursor()
+        table = check_page_and_return_table(request)
+        if request.method == 'POST':
+            form = AccessFormInput(request.POST, extra=table.pk)
+            if form.is_valid():
+                for (attribute, value) in form.extra_attributes():
+                    table.args.append([attribute, value])
+                table.args = reorder_dictionary(table)
+                delete_data(cursor, table)
+                name = create_reverse_name_remove(table.tname)
+                return redirect(reverse(name), permanent=True)
+        form = AccessFormInput(extra=table.pk)
+        list_of_data = select_data(cursor, table.tname)
+        args = get_args(table.cols)
+        context = create_context(table.tname, form, list_of_data, args)
+        return render(request, 'curator/remove.html', context)
+    else:
+        return redirect(login_page, permanent=True)
 
 
 def edit_data_page(request):
-    cursor = connection.cursor()
-    table = check_page_and_return_table(request)
-    if request.method == 'POST':
-        form = AccessFormInput(request.POST, extra=table.cols)
-        if form.is_valid():
-            for (attribute, value) in form.extra_attributes():
-                table.args.append([attribute, value])
-            table.args = reorder_dictionary(table)
-            table.non_pk_args = get_non_pk_args(table)
-            edit_data(cursor, table)
-            return redirect(edit_data_page, permanent=True)
-    form = AccessFormInput(extra=table.cols)
-    list_of_data = select_data(cursor, table.tname)
-    args = get_args(table.cols)
-    context = create_context(table.tname, form, list_of_data, args)
-    return render(request, 'curator/edit.html', context)
+    if request.user.is_staff or request.user.is_superuser:
+        cursor = connection.cursor()
+        table = check_page_and_return_table(request)
+        if request.method == 'POST':
+            form = AccessFormInput(request.POST, extra=table.cols)
+            if form.is_valid():
+                for (attribute, value) in form.extra_attributes():
+                    table.args.append([attribute, value])
+                table.args = reorder_dictionary(table)
+                table.non_pk_args = get_non_pk_args(table)
+                edit_data(cursor, table)
+                name = create_reverse_name_edit(table.tname)
+                return redirect(reverse(name), permanent=True)
+        form = AccessFormInput(extra=table.cols)
+        list_of_data = select_data(cursor, table.tname)
+        args = get_args(table.cols)
+        context = create_context(table.tname, form, list_of_data, args)
+        return render(request, 'curator/edit.html', context)
+    else:
+        return redirect(login_page, permanent=True)
 
 
 def login_page(request):
-    return
+    context = {
+        "welcome": "You should login.",
+    }
+    return render(request, "curator/login.html", context)
 
 
-def form_home(request):
-    if request.user.is_authenticated():
+def curator_home(request):
+    if request.user.is_staff or request.user.is_superuser:
         context = {
             "welcome": "Welcome to the curator's home page.",
         }
@@ -82,3 +98,5 @@ def form_home(request):
             "welcome": "You are not authorized to view this page.",
         }
     return render(request, "curator/index.html", context)
+
+# Create navigation bar for home page
